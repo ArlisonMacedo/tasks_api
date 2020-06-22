e682462581009ee525e8c12645f79dd10acde546
ArlisonMacedo <arlisonmacedo526@gmail.com>
2020-06-22 01:21:13 -0300

    Build

---
+++ dist/src/routes.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var UserController_1 = require("./controllers/UserController");
+var TaskController_1 = require("./controllers/TaskController");
+var routes = express_1.default.Router();
+routes.post('/v1/api/users', UserController_1.store);
+routes.get('/v1/api/tasks', TaskController_1.taskIndex);
+routes.post('/v1/api/tasks', TaskController_1.taskStore);
+routes.get('/v1/api/tasks/completed', TaskController_1.taksIndexCompleted);
+routes.put('/v1/api/tasks/:id', TaskController_1.taksCompleted);
+routes.delete('/v1/api/tasks/:id', TaskController_1.taskDelete);
+exports.default = routes;
